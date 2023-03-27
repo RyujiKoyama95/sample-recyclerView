2c19abc80d348eb8089c252c49397d6816b20bfa
RyujiKoyama95 <ryujikoyama95@gmail.com>
2023-03-27 09:15:56 +0900

    Add code for using recycler view

---
+++ app/src/main/java/com/uminari/samplerecyclerview/MainAdapter.kt
@@ -0,0 +1,28 @@
+package com.uminari.samplerecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MainAdapter(
+    private val list: List<Item>
+): RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return MainViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        holder.textView.text = list[position].text
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class MainViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val textView: TextView = itemView.findViewById(R.id.list_item_text_view)
+    }
+}
