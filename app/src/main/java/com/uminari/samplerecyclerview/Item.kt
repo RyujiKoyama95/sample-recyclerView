083a20f46169f86d259e8c6f9cde03153455792c
RyujiKoyama95 <ryujikoyama95@gmail.com>
2023-03-27 23:15:51 +0900

    Add code to MainFragment for recycler view

--- app/src/main/java/com/uminari/samplerecyclerview/Item.kt
+++ app/src/main/java/com/uminari/samplerecyclerview/Item.kt
@@ -1,5 +1,5 @@
 package com.uminari.samplerecyclerview
 
 data class Item(
-    val text: String = ""
+    var text: String = ""
 )
--- app/src/main/java/com/uminari/samplerecyclerview/MainFragment.kt
+++ app/src/main/java/com/uminari/samplerecyclerview/MainFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainFragment : Fragment() {
+    private var recyclerView: RecyclerView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,5 +22,25 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = view.findViewById(R.id.container_recycler_view)
+        recyclerView?.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            itemAnimator = DefaultItemAnimator()
+            adapter = MainAdapter(
+                getList()
+            )
+        }
+    }
+
+    private fun getList(): List<Item> {
+        val list = mutableListOf<Item>()
+        for (i in 0..100) {
+            val item = Item()
+            item.text = "${i}行目"
+            list.add(item)
+        }
+        return list
     }
 }
