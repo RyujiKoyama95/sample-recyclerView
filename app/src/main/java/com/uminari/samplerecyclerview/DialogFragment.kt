60af93513067778e061494929b0580e35e56565f
RyujiKoyama95 <ryujikoyama95@gmail.com>
2023-04-04 23:06:38 +0900

    Add backstack

--- app/src/main/java/com/uminari/samplerecyclerview/DialogFragment.kt
+++ app/src/main/java/com/uminari/samplerecyclerview/DialogFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 
 class DialogFragment : Fragment() {
     override fun onCreateView(
@@ -16,5 +18,10 @@ class DialogFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.add_button)
+        button.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
     }
 }
